0cc6710d674fcfeceed32d03594eba74b4689cc8
Jason Snow <jsn.snw@gmail.com>
2019-09-12 22:00:52 -0400

    Fix assertion findings

--- tests/test_youversion.py
+++ tests/test_youversion.py
@@ -21,11 +21,13 @@ class APITest(unittest.TestCase):
 
     def test_set_valid_language(self):
         self.client.language = youversion.Language.Spanish
-        assert self.client.language == youversion.Language.Spanish
+        if not self.client.language == youversion.Language.Spanish:
+            raise AssertionError()
 
     def test_set_valid_language_abbreviation(self):
         self.client.language = 'es'
-        assert self.client.language == youversion.Language.Spanish
+        if not self.client.language == youversion.Language.Spanish:
+            raise AssertionError()
 
     def test_set_invalid_language(self):
         with pytest.raises(youversion.UnsupportedLanguage):
@@ -33,18 +35,24 @@ class APITest(unittest.TestCase):
 
     def test_header_accepts_json(self):
         header = self.client._header
-        assert 'accept' in header
-        assert header['accept'] == 'application/json'
+        if 'accept' not in header:
+            raise AssertionError()
+        if not header['accept'] == 'application/json':
+            raise AssertionError()
 
     def test_header_contains_token(self):
         header = self.client._header
-        assert 'x-youversion-developer-token' in header
-        assert header['x-youversion-developer-token'] == YOUVERSION_API_TOKEN
+        if 'x-youversion-developer-token' not in header:
+            raise AssertionError()
+        if not header['x-youversion-developer-token'] == YOUVERSION_API_TOKEN:
+            raise AssertionError()
 
     def test_header_contains_language(self):
         header = self.client._header
-        assert 'accept-language' in header
-        assert header['accept-language'] == youversion.Language.English
+        if 'accept-language' not in header:
+            raise AssertionError()
+        if not header['accept-language'] == youversion.Language.English:
+            raise AssertionError()
 
     def test_invalid__get_raises(self):
         with pytest.raises(HTTPError):
@@ -52,62 +60,81 @@ class APITest(unittest.TestCase):
 
     def test_default_bible_version(self):
         bible_version = self.client.bible_version
-        assert bible_version.id is not None
-        assert bible_version.id == youversion.BibleVersion.KJV().id
+        if bible_version.id is None:
+            raise AssertionError()
+        if not bible_version.id == youversion.BibleVersion.KJV().id:
+            raise AssertionError()
 
     def test_valid_bible_version(self):
         self.client.bible_version = self.client.bible_versions['ASV']
-        assert self.client.bible_version.abbreviation == 'ASV'
+        if not self.client.bible_version.abbreviation == 'ASV':
+            raise AssertionError()
 
     def test_valid_bible_version_by_abbreviation(self):
         self.client.bible_version = 'ASV'
-        assert self.client.bible_version.abbreviation == 'ASV'
+        if not self.client.bible_version.abbreviation == 'ASV':
+            raise AssertionError()
 
     def test_invalid_bible_version(self):
         with pytest.raises(youversion.InvalidBibleVersion):
             self.client.bible_version = 'BAD_VERSION'
 
     def test_supports_valid_bible_version(self):
-        assert self.client.supports_bible_version('ASV') is True
+        if self.client.supports_bible_version('ASV') is False:
+            raise AssertionError()
 
     def test_does_not_support_invalid_bible_version(self):
-        assert self.client.supports_bible_version('BAD_VERSION') is False
+        if self.client.supports_bible_version('BAD_VERSION') is True:
+            raise AssertionError()
 
     def test_get_bible_versions(self):
-        assert len(self.client.bible_versions) > 0
+        if len(self.client.bible_versions) <= 0:
+            raise AssertionError()
 
     def test_get_bible_versions_contains_KJV(self):
-        assert 'KJV' in self.client.bible_versions
+        if 'KJV' not in self.client.bible_versions:
+            raise AssertionError()
 
     def test_get_valid_bible_version(self):
-        assert self.client.get_bible_version('ASV') is not None
+        if self.client.get_bible_version('ASV') is None:
+            raise AssertionError()
 
     def test_get_invalid_bible_version(self):
         with pytest.raises(youversion.InvalidBibleVersion):
             self.client.get_bible_version('BAD_VERSION')
 
     def test_get_verse_of_the_day(self):
-        assert self.client.get_verse_of_the_day() is not None
+        if self.client.get_verse_of_the_day() is None:
+            raise AssertionError()
 
     def test_get_verse_of_the_day_returns_request_day(self):
-        assert self.client.get_verse_of_the_day(day=90).day == 90
+        if not self.client.get_verse_of_the_day(day=90).day == 90:
+            raise AssertionError()
 
     def test_get_verse_of_the_day_bible_version(self):
         self.client.bible_version = 'ASV'
-        assert self.client.get_verse_of_the_day().bible_version.abbreviation == 'ASV'
+        if not self.client.get_verse_of_the_day().bible_version.abbreviation == 'ASV':
+            raise AssertionError()
 
     def test_get_verse_of_the_day_contains_verse(self):
         votd = self.client.get_verse_of_the_day()
-        assert votd.verse is not None
-        assert votd.verse.text is not None and votd.verse.text != ''
-        assert votd.verse.url is not None and votd.verse.url != ''
+        if votd.verse is None:
+            raise AssertionError()
+        if votd.verse.text is None or votd.verse.text == '':
+            raise AssertionError()
+        if votd.verse.url is None or votd.verse.url == '':
+            raise AssertionError()
 
     def test_get_verse_of_the_day_verse_bible_version(self):
         self.client.bible_version = 'ASV'
-        assert self.client.get_verse_of_the_day().verse.bible_version.abbreviation == 'ASV'
+        if not self.client.get_verse_of_the_day().verse.bible_version.abbreviation == 'ASV':
+            raise AssertionError()
 
     def test_get_verse_of_the_day_contains_image(self):
         votd = self.client.get_verse_of_the_day()
-        assert votd.image is not None
-        assert votd.image.verse is not None
-        assert votd.image._url is not None and votd.image._url != ''
+        if votd.image is None:
+            raise AssertionError()
+        if votd.image.verse is None:
+            raise AssertionError()
+        if votd.image._url is None or votd.image._url == '':
+            raise AssertionError()
